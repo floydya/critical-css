2f5cc8914876ab4172452c7696e567ce513009b6
Vlad Galatskiy <xfloydya@gmail.com>
2019-02-06 15:25:31 +0200

    pipenv to requirements

--- Makefile
+++ Makefile
@@ -2,5 +2,11 @@ start:
 	gunicorn app.wsgi -b 0.0.0.0:8000 -c app/gunicorn.py
 
 setup:
+	apt-get install rabbitmq-server
 	npm install
-	pipenv install
+	pip install -r requirements.txt
+
+requirements:
+	pipenv shell
+	pip freeze > requirements.txt
+	deactivate
--- app/settings.py
+++ app/settings.py
@@ -34,7 +34,9 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -82,3 +84,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
